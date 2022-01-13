7849335eb38821addc6d742574188a808773ea75
Divvu1103 <200110075@hbtu.ac.in>
2022-01-13 13:14:00 +0530

    added form

--- src/components/reactcontact.js
+++ src/components/reactcontact.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Reactcontact = () => {
     return (
         <>
-            <h1>hello</h1>
+        
+    <div>
+        <h1>Contact Us...!</h1>
+    </div>
+    <div class="container">
+        <p>Full Name: <input type="text" placeholder="Full Name" required/></p>
+        <p>E-mail:  <input type="email" placeholder="abcd@gmail.com" required/></p>
+        <p>Subject: <input type="text"placeholder="Job Enquiry" required/> </p>
+        <p>Message: <input type="text"name="Message"placeholder="Message" required/> </p>
+        <input type="submit" value="Submit"/>
+    </div>
+    
         </>
     )
 }
